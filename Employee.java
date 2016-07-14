1ee79ff8bb217118eaa303de05fc4f518f270108
ventsy95 <ventsy95@mail.bg>
2016-07-14 12:12:26 +0300

    Added 3 constants + method calculateOvertime

--- Employee.java
+++ Employee.java
@@ -1,11 +1,22 @@
 
 public class Employee extends Person {
+	private static final double COEFFICIENT_FOR_EXTRA_HOURS = 1.5;
+	private static final int WORKING_SHIFT_HOURS = 8;
+	private static final int LEGAL_AGE_FOR_WORK = 18;
 	private double daySalary;
 
 	Employee(String name, int age, boolean isMale, double daySalary) {
 		super(name, age, isMale);
 		if (daySalary > 0) {
 			this.setDaySalary(daySalary);
+		}
+	}
+
+	double calculateOvertime(double hours) {
+		if (this.age < LEGAL_AGE_FOR_WORK) {
+			return 0;
+		} else {
+			return ((this.daySalary / WORKING_SHIFT_HOURS) * COEFFICIENT_FOR_EXTRA_HOURS) * hours;
 		}
 	}
 
