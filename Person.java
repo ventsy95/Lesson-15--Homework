91879e9b50dcb8a790e07e51e39ec84ec0a6c7b6
ventsy95 <ventsy95@mail.bg>
2016-07-14 11:25:27 +0300

    Added fields and constructor to Person

--- Person.java
+++ Person.java
@@ -1,4 +1,20 @@
 
 public class Person {
+	String name;
+	int age;
+	boolean isMale;
 
+	Person(String name, int age, boolean isMale) {
+		if (name != null && (!name.equals(""))) {
+			this.name = name;
+		}else{
+			System.out.println("Enter a valid name.");
+		}
+		if (age > 0) {
+			this.age = age;
+		}else{
+			System.out.println("Enter a valid age.");
+		}
+		this.isMale = isMale;
+	}
 }
