fa4c9906eac366a9a2b6c8333493f6add754f207
ventsy95 <ventsy95@mail.bg>
2016-07-14 11:41:01 +0300

    Added method showStudentInfo + getter/setter for Score

--- Student.java
+++ Student.java
@@ -1,12 +1,25 @@
 
 public class Student extends Person {
-	double score;
+	private double score;
 
 	Student(String name, int age, boolean isMale, double score) {
 		super(name, age, isMale);
 		if (score >= 2 && score <= 6) {
-			this.score = score;
+			this.setScore(score);
 		}
 	}
 
+	void showStudentInfo() {
+		super.showPersonInfo();
+		System.out.print(" | ");
+		System.out.println("Score: " + this.getScore());
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
 }
