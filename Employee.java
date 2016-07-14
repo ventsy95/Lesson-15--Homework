1dc1fe788ce6eeaa2e39b96065df4b2629b613f2
ventsy95 <ventsy95@mail.bg>
2016-07-14 11:47:34 +0300

    Added field and constructor to Employee class + getter/setter for daySalary

--- Employee.java
+++ Employee.java
@@ -1,4 +1,20 @@
 
-public class Employee {
+public class Employee extends Person {
+	private double daySalary;
+
+	Employee(String name, int age, boolean isMale, double daySalary) {
+		super(name, age, isMale);
+		if (daySalary > 0) {
+			this.setDaySalary(daySalary);
+		}
+	}
+
+	public double getDaySalary() {
+		return daySalary;
+	}
+
+	public void setDaySalary(double daySalary) {
+		this.daySalary = daySalary;
+	}
 
 }
