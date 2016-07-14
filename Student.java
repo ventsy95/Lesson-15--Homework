e7ead67ee80b6f0687d695fa25e7a98f76287268
ventsy95 <ventsy95@mail.bg>
2016-07-14 11:34:34 +0300

    Added fields and constructor to Student class

--- Student.java
+++ Student.java
@@ -1,4 +1,12 @@
 
-public class Student {
+public class Student extends Person {
+	double score;
+
+	Student(String name, int age, boolean isMale, double score) {
+		super(name, age, isMale);
+		if (score >= 2 && score <= 6) {
+			this.score = score;
+		}
+	}
 
 }
