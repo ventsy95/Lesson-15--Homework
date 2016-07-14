d3cb9a871cf5bc4565109123aa73f0d9cc72c2b6
ventsy95 <ventsy95@mail.bg>
2016-07-14 12:52:43 +0300

    Written Tasks 1, 2 and 3 in Demo class

--- Demo.java
+++ Demo.java
@@ -2,8 +2,31 @@
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Person[] people = new Person[10];
+		Person firstPerson = new Person("Joro", 20, true);
+		Person secondPerson = new Person("Ivanka", 22, false);
+		Student firstStudent = new Student("Mario", 21, true, 3.6);
+		Student secondStudent = new Student("Ivelina", 19, false, 4.78);
+		Employee firstEmployee = new Employee("Vasilka", 35, false, 15.90);
+		Employee secondEmployee = new Employee("Iliqn", 17, true, 13);
 
+		people[0] = firstPerson;
+		people[1] = secondPerson;
+		people[2] = firstStudent;
+		people[3] = secondStudent;
+		people[4] = firstEmployee;
+		people[5] = secondEmployee;
+
+		for (int index = 0; index < people.length; index++) {
+			if (people[index] instanceof Student) {
+				((Student) people[index]).showStudentInfo();
+			}
+			if (people[index] instanceof Employee) {
+				((Employee) people[index]).showEmployeeInfo();
+			} else {
+				people[index].showPersonInfo();
+			}
+		}
 	}
 
 }
